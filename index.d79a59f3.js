0358a6e7082493fb294842093f34b7fc1aa60b7b
svyatoslav1313 <svyatoslavkozelchuk@gmail.com>
2026-01-13 03:17:39 +0200

    make build

---
+++ index.d79a59f3.js
@@ -0,0 +1,2 @@
+var c=document.querySelectorAll(".gallery__nav-link");c.forEach(function(e){e.addEventListener("click",function(){c.forEach(function(c){return c.classList.remove("active")}),e.classList.add("active")})});
+//# sourceMappingURL=index.d79a59f3.js.map
